423b7052972b5529f798bbf3ff45335cece2bb6c
thijmen brand <thijmen@ik.nu>
2024-12-07 22:26:05 +0100

    format

--- terraform/azure-deployment/providers.tf
+++ terraform/azure-deployment/providers.tf
@@ -1,38 +1,38 @@
 terraform {
-    required_version = ">=1.0"
+  required_version = ">=1.0"
 
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~>3.0"
-        }
-        azapi = {
-            source = "azure/azapi"
-            version = "~>1.5"
-        }
-        random = {
-            source = "hashicorp/random"
-            version = "~>3.0"
-        }
-        azuread = {
-            source = "hashicorp/azuread"
-            version = "2.30.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+  }
 
-    backend "azurerm" {
-      key = "terraform.tfstate"
-      use_oidc = true
-    }
+  backend "azurerm" {
+    key      = "terraform.tfstate"
+    use_oidc = true
+  }
 }
 
 provider "azurerm" {
-    features {}
-    use_oidc = true
+  features {}
+  use_oidc = true
 }
 
 provider "azapi" {
-    use_oidc = true
+  use_oidc = true
 }
 
 provider "azuread" {}
